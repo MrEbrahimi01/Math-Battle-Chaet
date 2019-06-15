0163e673d4261e8ecdd0feb697a1e2b457539262
MrEbrahimi01 <mre.pc01@gmail.com>
2019-06-15 15:49:12 +0430

    C# Project to Cheat in Math Battle Game ( Telegram ) .

---
+++ Math Battle Cheat/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Math_Battle_Cheat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txt_link = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(347, 16);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(40, 46);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(524, 472);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // txt_link
+            // 
+            this.txt_link.Font = new System.Drawing.Font("IRANSans Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_link.Location = new System.Drawing.Point(9, 33);
+            this.txt_link.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.txt_link.Name = "txt_link";
+            this.txt_link.Size = new System.Drawing.Size(300, 37);
+            this.txt_link.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_link);
+            this.groupBox2.Location = new System.Drawing.Point(20, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(318, 81);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Link";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Enabled = false;
+            this.btn_start.Location = new System.Drawing.Point(124, 434);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(96, 54);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Enabled = false;
+            this.btn_stop.Location = new System.Drawing.Point(226, 434);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(96, 54);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 54);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 500);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btn_start);
+            this.Font = new System.Drawing.Font("IRANSans Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Math Battle Cheat | Coded By Mohammad Reza Ebrahimi | Telegram : @Mr_Ebrahimi01";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txt_link;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Math Battle Cheat/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Math_Battle_Cheat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.GetElementById("button_correct").InvokeMember("Click");
+            timer1.Start();
+            btn_stop.Enabled = true;
+            btn_start.Enabled = false;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                webBrowser1.Url = new System.Uri(txt_link.Text, System.UriKind.Absolute);
+                btn_start.Enabled = true;
+                button1.Enabled = false;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("لطفا لینک معتبر وارد کنید .");
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cheat();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btn_start.Enabled = false;
+            btn_stop.Enabled = false;
+            button1.Enabled = true;
+        }
+        public void cheat()
+        {
+            int num1 = Convert.ToInt32(webBrowser1.Document.GetElementById("task_x").InnerHtml);
+            int num2 = Convert.ToInt32(webBrowser1.Document.GetElementById("task_y").InnerHtml);
+            int result = Convert.ToInt32(webBrowser1.Document.GetElementById("task_res").InnerHtml);
+            string op = Convert.ToString(webBrowser1.Document.GetElementById("task_op").InnerHtml);
+
+            switch (op)
+            {
+                case "/":
+                    {
+                        if (num1 / num2 == result)
+                        {
+                            webBrowser1.Document.GetElementById("button_correct").InvokeMember("Click");
+                        }
+                        else
+                        {
+                            webBrowser1.Document.GetElementById("button_wrong").InvokeMember("Click");
+                        }
+                    }
+                    break;
+                case "+":
+                    {
+                        if (num1 + num2 == result)
+                        {
+                            webBrowser1.Document.GetElementById("button_correct").InvokeMember("Click");
+                        }
+                        else
+                        {
+                            webBrowser1.Document.GetElementById("button_wrong").InvokeMember("Click");
+                        }
+                    }
+                    break;
+                case "–":
+                    {
+                        if (num1 - num2 == result)
+                        {
+                            webBrowser1.Document.GetElementById("button_correct").InvokeMember("Click");
+                        }
+                        else
+                        {
+                            webBrowser1.Document.GetElementById("button_wrong").InvokeMember("Click");
+                        }
+                    }
+                    break;
+                case "×":
+                    {
+                        if (num1 * num2 == result)
+                        {
+                            webBrowser1.Document.GetElementById("button_correct").InvokeMember("Click");
+                        }
+                        else
+                        {
+                            webBrowser1.Document.GetElementById("button_wrong").InvokeMember("Click");
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
